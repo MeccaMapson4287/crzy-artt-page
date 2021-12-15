0d723fdda8d0ccccb7cef2ab3e42ea76e03b7bee
MeccaMapson4287 <71038971+MeccaMapson4287@users.noreply.github.com>
2021-12-15 18:56:40 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
